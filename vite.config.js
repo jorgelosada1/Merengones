76b30f449acec78bb2767ec4e0928f7e83f42f1b
Jorge Losada <jorgeandresvargas123@gmail.com>
2025-01-19 20:48:03 -0500

    modify Viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',  // Permite acceder desde la red local
-    port: 2000,        // Puerto 2000
-  },
-});
+})
